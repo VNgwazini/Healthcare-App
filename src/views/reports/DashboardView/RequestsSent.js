982a61fc44eced8d6b2de14e4d726b9ba96d44dc
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-07-07 12:20:14 -0700

    Dashboard requests now pull from data.js

--- src/views/reports/DashboardView/RequestsSent.js
+++ src/views/reports/DashboardView/RequestsSent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import { v4 as uuid } from 'uuid';
@@ -17,114 +17,78 @@ import {
   TableHead,
   TableRow
 } from '@material-ui/core';
+import { 
+  bloodRequests,
+  bloodBanks
+} from  "../../../data"
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
-class RequestsSent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      requests: []
-    }
-  }
+const RequestsSent = ({ className, requests, ...rest }) => {
 
-  componentDidMount() {
-    const token = localStorage.getItem("jwt");
-    const user = JSON.parse(localStorage.getItem("user"));
-    
-    const getData = async () => {
-      var url = `http://localhost:1337/blood-requests?requestor.id=${user.bloodBank.id}&supplier.id_ne=${user.bloodBank.id}&status_ne=canceled&_sort=createdAt:DESC&_limit=5`;
-      const response = await axios.get(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      this.setState({
-        requests: response.data
-      });
-    }
-    if (user) {
-      getData();
-    }
-  }
-
-  render() {
-    const {className, ...rest} = this.props;
-
-    var data = [];
-    for (var i in this.state.requests) {
-      var req = this.state.requests[i];
-      data.push({
-        id: uuid(),
-        supplier: {
-          name: req.supplier ? req.supplier.name : "Finding supplier..."
-        },
-        type: req.bloodGroup,
-        units: req.units,
-        createdAt: req.createdAt,
-        status: req.status,
-        requestType: req.requestType
-      });
-    }
-
-    const requests = data;
-
-    return (
-      <Card className={className} {...rest}>
-        <CardHeader title="Requests Sent" />
-        <Divider />
-        <PerfectScrollbar>
-          <Box minWidth={800}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell sortDirection="desc">Date
-                      {/* <Tooltip enterDelay={300} title="Sort">
-                        <TableSortLabel active direction="desc">
-                          Date
-                        </TableSortLabel>
-                      </Tooltip> */}
-                    </TableCell>
-                  <TableCell>Supplier</TableCell>
-                  <TableCell>Type</TableCell>
-                  <TableCell>Units</TableCell>
-                  <TableCell>Request Type</TableCell>
-                  <TableCell>Status</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {requests.map(request => (
-                  <TableRow hover key={request.id}>
-                    <TableCell>
-                      {moment(request.createdAt).format('ll')}
-                    </TableCell>
-                    <TableCell>{request.supplier.name}</TableCell>
-                    <TableCell>{request.type}</TableCell>
-                    <TableCell>{request.units}</TableCell>
-                    <TableCell>{request.requestType}</TableCell>
-                    <TableCell>
-                      <Chip color="primary" label={request.status} size="small" />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Box>
-        </PerfectScrollbar>
-        <Box display="flex" justifyContent="flex-end" p={2}>
-          <Button
-            color="primary"
-            endIcon={<ArrowRightIcon />}
-            size="small"
-            variant="text"
-            href="http://localhost:3000/app/manage_requests"
-          >
-            View all
-          </Button>
+  const [requestData, setrequestData] = useState(bloodRequests);
+  const [bankData, setbankData] = useState(bloodBanks);
+  return (
+    <Card className={className} {...rest}>
+      <CardHeader title="Requests Sent" />
+      <Divider />
+      <PerfectScrollbar>
+        <Box minWidth={800}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sortDirection="desc">Date
+                    {/* <Tooltip enterDelay={300} title="Sort">
+                      <TableSortLabel active direction="desc">
+                        Date
+                      </TableSortLabel>
+                    </Tooltip> */}
+                  </TableCell>
+                <TableCell>Requestor</TableCell>
+                <TableCell>Type</TableCell>
+                <TableCell>Units</TableCell>
+                <TableCell>Request Type</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {requestData
+              .slice(0,9)
+              .map((request, index) => (
+                <TableRow hover key={request.id}>
+                <TableCell>
+                  {moment(request.createdAt).format('ll')}
+                </TableCell>
+                <TableCell>
+                  {console.log('bankdata[', index, '] = ', bankData[index])}
+                {bankData[index%bankData.length].name ? bankData[index%bankData.length].name + " County Hospital" : "Finding supplier..."}
+                </TableCell>
+                <TableCell>{request.requestType}</TableCell>
+                <TableCell>{request.units}</TableCell>
+                <TableCell>{request.requestType}</TableCell>
+                <TableCell>
+                  <Chip color="primary" label={request.status} size="small" />
+                </TableCell>
+              </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </Box>
-      </Card>
-    );
-  }
-};
+      </PerfectScrollbar>
+      <Box display="flex" justifyContent="flex-end" p={2}>
+        <Button
+          color="primary"
+          endIcon={<ArrowRightIcon />}
+          size="small"
+          variant="text"
+          href="http://localhost:3000/app/manage_requests"
+        >
+          View all
+        </Button>
+      </Box>
+    </Card>
+  );
+}
+
 
 RequestsSent.propTypes = {
   className: PropTypes.string
